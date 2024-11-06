5a8fe997819a5c14c78b96466a2d44847ce8efbd
Mohit Singh <mohitsingh20.dev@gmail.com>
2024-11-06 13:41:53 +0530

    added InvestorService

---
+++ Prequin.Microservices/InvestorService/Enums/InvestorType.cs
@@ -0,0 +1,10 @@
+﻿namespace InvestorService.Enums
+{
+    public enum InvestorType
+    {
+        Bank = 1,
+        AssetManager = 2,
+        FundManager = 3,
+        WealthManager = 4
+    }
+}
